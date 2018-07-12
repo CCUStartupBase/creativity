dd5a74aff7049ec5245534770c7873a1f2489cb9
Vincent Huang <vincent0740@gmail.com>
2018-07-12 09:24:30 +0000

    database schema(News table) & add editor config

--- database/migrations/2018_07_12_075924_create_news_table.php
+++ database/migrations/2018_07_12_075924_create_news_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateNewsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('news', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title', 50);
+            $table->longText('content');
+            $table->string('source', 20);
+            $table->string('source_url', 100)->nullable();
+            $table->boolean('visibled')->default(false);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -30,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('news');
     }
 }
