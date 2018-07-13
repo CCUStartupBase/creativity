edf3a31fbc0db755b0572bc2c9ad11fe29c41740
yao-ching <wang7186wang@gmail.com>
2018-07-13 16:48:20 +0800

    migrate table

---
+++ database/migrations/2018_07_13_082904_create_event_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 50);
+            $table->longText('content');
+            $table->string('source', 20);
+            $table->string('position', 20);            
+            $table->date('begin_date');
+            $table->date('end_date');
+            $table->integer('quota');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event');
+    }
+}
